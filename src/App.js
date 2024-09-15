aa0a0b568810d11229d4a96413b6084cc8d4fc83
smithofbabylonia <martinmalaza59@gmail.com>
2024-09-15 11:43:56 +0200

    The network monitor is unusable.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import Chart from 'chart.js/auto';
 
-import Temperature from './components/Temperature';
 import Storage from './components/Storage';
 import Ram from './components/Ram';
 import ServerApps from './components/ServerApps';
-import NetworkActivity from './components/NetworkActivity';
 import CrescentTemperature from './components/CrescentTemperature';
 
 function App() {
-  const server_link = "http://192.168.1.101:5000/api";
+  const server_link = "http://127.0.1.1:5000/api";
   return (
     <div className="App">
       <h1>Dashboard</h1>
@@ -20,7 +17,6 @@ function App() {
           <CrescentTemperature title="Temp 2" server={server_link}/>
           <Storage server={server_link}/>
           <Ram server={server_link}/>
-          <NetworkActivity server={server_link}/>
         </div>
         
         <div className="section apps">
